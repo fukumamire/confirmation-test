16e399d9bb4ffc76148744be0c7ae91963155509
fukumamire <rinka_karyou@hotmail.com>
2024-03-02 12:21:30 +0900

    確認テスト　コントローラーｐｈｐ作成後のルート設定　

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// お問い合わせフォーム入力ページ
+Route::get('/', [ContactController::class, 'form']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// お問い合わせフォーム確認ページ
+Route::get('/confirm', [ContactController::class, 'confirm']);
+
+// サンクスページ
+Route::get('/thanks', [ContactController::class, 'thanks']);
+
+// 管理画面
+Route::get('/admin', [AdminController::class, 'dashboard']);
+
+// ユーザ登録ページ
+Route::get('/register', [AuthController::class, 'register']);
+
+// ログインページ
+Route::get('/login', [AuthController::class, 'login']);
