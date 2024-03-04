528ade9a06ad3d800490dffbc39f70cc5428e281
fukumamire <rinka_karyou@hotmail.com>
2024-03-04 22:36:23 +0900

    お問い合わせ確認画面　comfirm.blade.php　の編集　８項目をテーブルで表示させた<td>等　

--- src/resources/views/contact/confirm.blade.php
+++ src/resources/views/contact/confirm.blade.php
@@ -10,42 +10,45 @@
     <h2>Confirm</h2>
     <form action="/thanks" method="post">
         @csrf
-        <div>
-            <label>お名前</label>
-            <p>{{ $contact['last_name'] ?? '' }} {{ $contact['first_name'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>性別</label>
-            <p>{{ $contact['gender'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>メールアドレス</label>
-            <p>{{ $contact['email'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>電話番号</label>
-            <p>{{ $contact['area_code'] ?? '' }}-{{ $contact['local_code'] ?? '' }}-{{ $contact['phone_number'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>住所</label>
-            <p>{{ $contact['address'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>建物名</label>
-            <p>{{ $contact['building_name'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>お問い合わせの種類</label>
-            <p>{{ $contact['inquiry_type'] ?? '' }}</p>
-        </div>
-        <div>
-            <label>お問い合わせ内容</label>
-            <p>{{ $contact['inquiry_content'] ?? '' }}</p>
-        </div>
-        <div>
+        <table>
+            <tr>
+                <th>お名前</th>
+                <td>{{ $contact['last_name'] ?? '' }} {{ $contact['first_name'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>性別</th>
+                <td>{{ $contact['gender'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>メールアドレス</th>
+                <td>{{ $contact['email'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>電話番号</th>
+                <td>{{ $contact['area_code'] ?? '' }}-{{ $contact['local_code'] ?? '' }}-{{ $contact['phone_number'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>住所</th>
+                <td>{{ $contact['address'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>建物名</th>
+                <td>{{ $contact['building_name'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>お問い合わせの種類</th>
+                <td>{{ $contact['inquiry_type'] ?? '' }}</td>
+            </tr>
+            <tr>
+                <th>お問い合わせ内容</th>
+                <td>{{ $contact['inquiry_content'] ?? '' }}</td>
+            </tr>
+        </table>
+        <div class="buttons">
             <button type="submit">送信</button>
             <a href="/">修正</a>
         </div>
     </form>
 </div>
 @endsection
+        
